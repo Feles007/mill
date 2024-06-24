275a7b8d5c6c0678735f9132839dfb2d7583f487
Feles007 <75643325+Feles007@users.noreply.github.com>
2024-06-24 14:53:47 -0500

    Move example to a separate file

--- src/main.rs
+++ src/main.rs
@@ -17,48 +17,9 @@ type UInteger = u32;
 type Float = f64;
 
 fn main() {
-	let source = r#"
+	let source = std::fs::read_to_string("test.radio").unwrap();
 
-let x = 0;
-let y = 1;
-let z = x + y;
-
-loop {
-	print(x);
-	x = y;
-	y = z;
-	z = x + y;
-
-	if z > 255 && y != 0 {
-		break;
-	}
-}
-
-let m = {["bop"]: "asd"};
-
-for _ in {} {}
-
-for pair in std.map.kv(m) {
-	let k = pair.k;
-	let v = pair.v;
-
-	assert(m[k] == v);
-}
-
-let cond = true;
-while cond {
-	let f = fn (a) { return a * 3; };
-	if f(3) == 9 {
-		cond = false;
-	}
-	{
-		let x = 3;
-	}
-}
-
-"#;
-
-	let mut lexer = lexer::Lexer::new(source);
+	let mut lexer = lexer::Lexer::new(&source);
 
 	match statement::parse_block(&mut lexer, false) {
 		Ok(e) => println!("{e:#?}"),
