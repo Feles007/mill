4f06e05904af0b0285b90fde2305bbeadab6080a
Feles007 <75643325+Feles007@users.noreply.github.com>
2024-07-27 20:06:07 -0500

    Minor change to public api

--- radio-script/src/parser/mod.rs
+++ radio-script/src/parser/mod.rs
@@ -3,11 +3,10 @@ mod expression;
 mod lexer;
 mod statement;
 
-type LineNumber = usize;
-
-pub use error::ParseError;
-
 use crate::ast::Ast;
+
+pub type LineNumber = usize;
+pub use error::{ParseError, ParseErrorKind};
 
 pub fn parse<Source: AsRef<str>>(source: Source) -> Result<Ast, error::ParseError> {
 	let mut lexer = lexer::Lexer::new(source.as_ref());
